0bd6c5eb8d47e2bb770970f1fc9c23f790605a2d
Michael Kotliar <misha.kotliar@gmail.com>
2021-11-29 18:40:19 -0500

    Update cell type assignment script to export plots

--- tools/dockerfiles/scripts/assign_cell_types.R
+++ tools/dockerfiles/scripts/assign_cell_types.R
@@ -3,10 +3,16 @@ options(warn=-1)
 options("width"=200)
 options(future.globals.maxSize = 8000 * 1024^2)  # 8GB should be good by default
 
+suppressMessages(library(dplyr))
 suppressMessages(library(Seurat))
 suppressMessages(library(future))
+suppressMessages(library(ggplot2))
+suppressMessages(library(ggrepel))
 suppressMessages(library(argparse))
+suppressMessages(library(tidyverse))
 suppressMessages(library(data.table))
+suppressMessages(library(reticulate))
+suppressMessages(library(RColorBrewer))
 
 
 set_threads <- function (threads) {
@@ -51,6 +57,352 @@ load_ctype_data <- function (location) {
 }
 
 
+writeSparseMatrix <- function (inMat, outFname, sliceSize=1000){
+    fnames <- c()
+    mat <- inMat
+    geneCount <- nrow(mat)
+    startIdx <- 1
+    while (startIdx < geneCount){
+        endIdx <- min(startIdx + sliceSize - 1, geneCount)
+        matSlice <- mat[startIdx:endIdx,]
+        denseSlice <- as.matrix(matSlice)
+        dt <- data.table(denseSlice)
+        dt <- cbind(gene=rownames(matSlice), dt)
+        writeHeader <- FALSE
+        if (startIdx == 1) { 
+            writeHeader <- TRUE
+        }
+        sliceFname <- paste0("temp", startIdx, ".txt")
+        fwrite(dt, sep="\t", file=sliceFname, quote=FALSE, col.names=writeHeader)
+        fnames <- append(fnames, sliceFname)
+        startIdx <- startIdx + sliceSize
+    }
+    system(paste("cat", paste(fnames, collapse=" "), "| gzip >", outFname, sep=" "))
+    unlink(fnames)
+}
+
+
+findMatrix <- function(object, matrix.slot){
+    if (matrix.slot == "counts") {
+        counts <- GetAssayData(object=object, slot="counts")
+    } else if (matrix.slot == "scale.data") {
+        counts <- GetAssayData(object=object, slot="scale.data")
+    } else if (matrix.slot == "data") {
+        counts <- GetAssayData(object=object)
+    } else {
+        error("matrix.slot can only be one of: counts, scale.data, data")
+    }
+}
+
+
+ExportToCellbrowser <- function(
+    object,
+    matrix.slot,
+    dir,
+    cluster.field,
+    features,
+    meta.fields,
+    meta.fields.names
+) {
+    if (!dir.exists(dir)) {
+        dir.create(dir)
+    }
+    idents <- Idents(object)
+    meta <- object@meta.data
+    cellOrder <- colnames(object)
+    counts <- findMatrix(object, matrix.slot)
+    genes <- rownames(x=object)
+    dr <- object@reductions
+
+    gzPath <- file.path(dir, "exprMatrix.tsv.gz")
+    if ((((ncol(counts)/1000)*(nrow(counts)/1000))>2000) && is(counts, 'sparseMatrix')){
+        writeSparseMatrix(counts, gzPath);
+    } else {
+        mat <- as.matrix(counts)
+        df <- as.data.frame(mat, check.names=FALSE)
+        df <- data.frame(gene=genes, df, check.names=FALSE)
+        z <- gzfile(gzPath, "w")
+        write.table(x=df, sep="\t", file=z, quote=FALSE, row.names=FALSE)
+        close(con=z)
+    }
+    embeddings = names(dr)
+    embeddings.conf <- c()
+    for (embedding in embeddings) {
+        emb <- dr[[embedding]]
+        df <- emb@cell.embeddings
+        if (ncol(df) > 2){
+            df <- df[, 1:2]
+        }
+        colnames(df) <- c("x", "y")
+        df <- data.frame(cellId=rownames(df), df, check.names=FALSE)
+        write.table(
+            df[cellOrder,],
+            sep="\t",
+            file=file.path(dir, sprintf("%s.coords.tsv", embedding)),
+            quote=FALSE,
+            row.names=FALSE
+        )
+        embeddings.conf <- c(
+            embeddings.conf,
+            sprintf('{"file": "%s.coords.tsv", "shortLabel": "Seurat %1$s"}', embedding)
+        )
+    }
+    df <- data.frame(row.names=cellOrder, check.names=FALSE)
+    enum.fields <- c()
+    for (i in 1:length(meta.fields)){
+        field <- meta.fields[i]
+        name <- meta.fields.names[i]
+        if (field %in% colnames(meta)){
+            df[[name]] <- meta[[field]]
+            if (!is.numeric(df[[name]])) {
+                enum.fields <- c(enum.fields, name)
+            }
+        }
+    }
+    df <- data.frame(Cell=rownames(df), df, check.names=FALSE)
+    write.table(
+        as.matrix(df[cellOrder,]),
+        sep="\t",
+        file=file.path(dir, "meta.tsv"),
+        quote=FALSE,
+        row.names=FALSE
+    )
+    if (!is.null(features)){
+        write.table(
+            features,
+            sep="\t",
+            file=file.path(dir, "quickGenes.tsv"),
+            quote=FALSE,
+            row.names=FALSE,
+            col.names=FALSE
+        )
+    }
+    config <- '
+name="cellbrowser"
+shortLabel="cellbrowser"
+exprMatrix="exprMatrix.tsv.gz"
+meta="meta.tsv"
+geneIdType="auto"
+clusterField="%s"
+labelField="%s"
+enumFields=%s
+coords=%s'
+    enum.string <- paste0("[", paste(paste0('"', enum.fields, '"'), collapse=", "), "]")
+    coords.string <- paste0("[", paste(embeddings.conf, collapse = ",\n"), "]")
+    config <- sprintf(
+        config,
+        cluster.field,
+        cluster.field,
+        enum.string,
+        coords.string
+    )
+    if (!is.null(features)){
+        config <- paste(config, 'quickGenesFile="quickGenes.tsv"', sep="\n")
+    }
+    confPath = file.path(dir, "cellbrowser.conf")
+    cat(config, file=confPath)
+    cb.dir <- paste(dir, "html_data", sep="/")
+    cb <- import(module = "cellbrowser")
+    cb$cellbrowser$build(dir, cb.dir)
+}
+
+
+export_cellbrowser_data <- function(seurat_data, suffix, args, assay="RNA", matrix_slot="data"){
+    # tryCatch(
+    #     expr = {
+            backup_assay <- DefaultAssay(seurat_data)
+            DefaultAssay(seurat_data) <- assay
+            meta_fields <- c("nCount_RNA", "nFeature_RNA", "log10_gene_per_log10_umi", "mito_percentage", "Phase", "S.Score", "G2M.Score")
+            meta_fields_names <- c("UMIs", "Genes", "Novelty score", "Mitochondrial %", "Cell cycle phase", "S score", "G to M score")
+            cluster_field <- "Cluster"
+            if ("integrated" %in% names(seurat_data@assays)) {
+                cluster_field <- "Identity"
+                meta_fields <- c(c("new.ident", "condition"), meta_fields)
+                meta_fields_names <- c(c("Identity", "Condition"), meta_fields_names)
+            }
+            meta_fields <- c(
+                meta_fields,
+                args$source,
+                args$target
+            )
+            meta_fields_names <- c(
+                meta_fields_names,
+                "Cluster",
+                "Cell Type"
+            )
+            rootname <- paste(args$output, suffix, sep="_")
+            ExportToCellbrowser(
+                seurat_data,
+                matrix.slot=matrix_slot,
+                dir=rootname,
+                cluster.field=cluster_field,
+                features=args$features,
+                meta.fields=meta_fields,
+                meta.fields.names=meta_fields_names
+            )
+            print(paste("Export UCSC Cellbrowser data to", rootname, sep=" "))
+    #     },
+    #     error = function(e){
+    #         print(paste("Failed to export UCSC Cellbrowser data to", rootname, sep=" "))
+    #     },
+    #     finally = {
+    #         DefaultAssay(seurat_data) <- backup_assay
+    #     }
+    # )
+}
+
+
+export_dim_plot <- function(data, rootname, reduction, plot_title, legend_title, split_by=NULL, group_by=NULL, perc_split_by=NULL, perc_group_by=NULL, label=FALSE, palette=NULL, pdf=FALSE, width=1200, height=800, resolution=100){
+    tryCatch(
+        expr = {
+            plot <- DimPlot(
+                        data,
+                        reduction=reduction,
+                        split.by=split_by,
+                        group.by=group_by,
+                        label=label
+                    ) +
+                    theme_gray() +
+                    ggtitle(plot_title) +
+                    guides(color=guide_legend(legend_title, override.aes=list(size=3)))
+
+            if (!is.null(palette)){ plot <- plot + scale_color_brewer(palette=palette) }
+
+            if(!is.null(perc_split_by) && !is.null(perc_group_by)){
+                width <- 2 * width
+                perc_data <- data@meta.data %>%
+                             dplyr::group_by(across(all_of(perc_split_by)), across(all_of(perc_group_by))) %>%
+                             summarise(counts=n(), .groups="drop_last") %>%               # drop the perc_group_by grouping level so we can get only groups defined by perc_split_by
+                             mutate(freq=counts/sum(counts)*100) %>%                      # sum is taken for the group defined by perc_split_by
+                             ungroup() %>%
+                             complete(
+                                 (!!as.symbol(perc_split_by)), (!!as.symbol(perc_group_by)),
+                                 fill=list(counts=0, freq=0)
+                             ) %>%
+                             arrange(all_of(perc_split_by), all_of(perc_group_by))        # sort for consistency
+                label_data <- data@meta.data %>%
+                              dplyr::group_by(across(all_of(perc_split_by))) %>%
+                              summarise(counts=n(), .groups="drop_last") %>%              # drops all grouping as we have only one level
+                              arrange(all_of(perc_split_by))                              # sort for consistency
+                perc_plot <- ggplot(perc_data, aes_string(x=perc_split_by, y="freq", fill=perc_group_by)) +
+                             geom_col(position="dodge", width=0.9, linetype="solid", color="black", show.legend=FALSE) +
+                             xlab("") +
+                             ylab("Cells percentage") +
+                             theme_gray() +
+                             geom_label_repel(
+                                label_data, mapping=aes(y=-Inf, label=counts),
+                                color="black", fill="white", segment.colour=NA,
+                                direction="y", size=3, show.legend=FALSE
+                             ) +
+                             RotatedAxis()
+                plot <- plot + perc_plot
+            }
+
+            png(filename=paste(rootname, ".png", sep=""), width=width, height=height, res=resolution)
+            suppressMessages(print(plot))
+            dev.off()
+
+            if (pdf) {
+                pdf(file=paste(rootname, ".pdf", sep=""), width=round(width/resolution), height=round(height/resolution))
+                suppressMessages(print(plot))
+                dev.off()
+            }
+
+            print(paste("Export dim plot to ", rootname, ".(png/pdf)", sep=""))
+        },
+        error = function(e){
+            tryCatch(expr={dev.off()}, error=function(e){print(paste("Called  dev.off() with error -", e))})
+            print(paste("Failed to export dim plot to ", rootname, ".(png/pdf)", sep=""))
+        }
+    )
+}
+
+
+export_all_clustering_plots <- function(seurat_data, suffix, args) {
+    cluster_prefix <- "RNA"
+    if ("integrated" %in% names(seurat_data@assays)) {
+        cluster_prefix <- "integrated"
+    }
+    export_dim_plot(
+        data=seurat_data,
+        reduction="umap",
+        plot_title="Grouped by cell type UMAP projected PCA of filtered integrated/scaled datasets",
+        legend_title="Cell type",
+        group_by=args$target,
+        perc_split_by="new.ident",
+        perc_group_by=args$target,
+        label=TRUE,
+        rootname=paste(args$output, suffix, "umap_ctype", sep="_"),
+        pdf=args$pdf
+    )
+    export_dim_plot(
+        data=seurat_data,
+        reduction="umap",
+        plot_title="Split by condition grouped by cell type UMAP projected PCA of filtered integrated/scaled datasets",
+        legend_title="Cell type",
+        split_by="condition",
+        group_by=args$target,
+        perc_split_by="condition",
+        perc_group_by=args$target,
+        label=TRUE,
+        rootname=paste(args$output, suffix, "umap_ctype_spl_by_cond", sep="_"),
+        pdf=args$pdf
+    )
+}
+
+
+export_all_expression_plots <- function(seurat_data, suffix, args, assay="RNA") {
+    backup_assay <- DefaultAssay(seurat_data)
+    DefaultAssay(seurat_data) <- assay
+    Idents(seurat_data) <- args$target
+    export_dot_plot(
+        data=seurat_data,
+        features=args$features,
+        plot_title="Scaled average log normalized gene expression per predicted cell type of filtered integrated/scaled datasets",
+        x_label="Genes",
+        y_label="Cell types",
+        cluster_idents=FALSE,  # no need to cluster cell types together
+        rootname=paste(args$output, suffix, "avg_per_ctype_res", sep="_"),
+        pdf=args$pdf
+    )
+    export_feature_plot(
+        data=seurat_data,
+        features=args$features,
+        labels=args$features,
+        reduction="umap",
+        plot_title="Log normalized gene expression per cell of clustered filtered integrated/scaled datasets with predicted cell types",
+        label=TRUE,
+        order=TRUE,
+        max_cutoff="q99",  # to prevent cells with overexpressed gene from distorting the color bar
+        rootname=paste(args$output, suffix, "per_ctype_cell_res", sep="_"),
+        combine_guides="keep",
+        pdf=args$pdf
+    )
+    export_expr_heatmap(
+        data=seurat_data,
+        features=args$features,
+        plot_title="Log normalized gene expression heatmap of clustered filtered integrated/scaled datasets with predicted cell types",
+        matrix_slot="data",  # LogNormalized version of the raw counts
+        palette=c("black", "orange"),
+        rootname=paste(args$output, suffix, "ctype_heatmap_res", sep="_"),
+        pdf=args$pdf
+    )
+    export_vln_plot(
+        data=seurat_data,
+        features=args$features,
+        labels=args$features,
+        plot_title="Log normalized gene expression densities per predicted cell type of filtered integrated/scaled datasets",
+        legend_title="Cell type",
+        log=TRUE,
+        pt_size=0,
+        combine_guides="collect",
+        rootname=paste(args$output, suffix, "dnst_per_ctype_res", sep="_"),
+        pdf=args$pdf
+    )
+    Idents(seurat_data) <- "new.ident"
+    DefaultAssay(seurat_data) <- backup_assay
+}
+
 
 get_args <- function(){
     parser <- ArgumentParser(description='Assigns cell types to clusters')
@@ -83,9 +435,19 @@ get_args <- function(){
         type="character", required="True"
     )
     parser$add_argument(
+        "--features",
+        help="Features of interest to highlight. Default: None",
+        type="character", nargs="*"
+    )
+    parser$add_argument(
         "--output",
-        help="Output prefix. Default: ./seurat_ctype",
-        type="character", default="./seurat_ctype"
+        help="Output prefix. Default: ./seurat",
+        type="character", default="./seurat"
+    )
+    parser$add_argument(
+        "--pdf",
+        help="Export plots in PDF. Default: false",
+        action="store_true"
     )
     parser$add_argument(
         "--threads",
@@ -113,5 +475,22 @@ seurat_data[[args$target]] <- ctype_data$type[match(
     as.vector(as.character(ctype_data$cluster)))
 ]
 
-print("Exporting results")
-export_rds(seurat_data, paste(args$output, "_clst_data.rds", sep=""))
+if (!is.null(args$features)){
+    print("Check genes of interest to include only those that are present in the datasets")
+    backup_assay <- DefaultAssay(seurat_data)
+    DefaultAssay(seurat_data) <- "RNA"
+    args$features <- unique(args$features)
+    args$features <- args$features[args$features %in% as.vector(as.character(rownames(seurat_data)))]
+    print(args$features)
+    DefaultAssay(seurat_data) <- backup_assay
+}
+
+print("Saving results")
+# export_rds(seurat_data, paste(args$output, "_ctype_data.rds", sep=""))
+export_all_clustering_plots(seurat_data, "clst", args)
+if (!is.null(args$features)){
+    export_all_expression_plots(seurat_data, "expr", args, assay="RNA")
+}
+
+print("Exporting UCSC Cellbrowser data")
+export_cellbrowser_data(seurat_data, "cellbrowser", args)
