5556e5dbed3957e6bd6ff7ad640824bc9a202c1f
Michael Kotliar <misha.kotliar@gmail.com>
2021-12-15 19:26:44 -0500

    Add --splitby parameter to extract_seurat_metadata.R script

--- tools/dockerfiles/scripts/extract_seurat_metadata.R
+++ tools/dockerfiles/scripts/extract_seurat_metadata.R
@@ -18,6 +18,40 @@ get_file_type <- function (filename) {
 }
 
 
+export_metadata <- function(data, suffix, args){
+    cells_data <- Cells(data)
+    print(paste("Saving", length(cells_data), "cell"))
+    print(head(cells_data))
+
+    umap_data <- Embeddings(data, reduction="umap")
+    print(paste("Saving", length(umap_data), "UMAP embeddings"))
+    print(head(umap_data))
+
+    cluster_data <- data@meta.data[, args$source]
+    print(paste("Saving", length(cluster_data), "clusters"))
+    print(head(cluster_data))
+
+    export_data(
+        cells_data,
+        location=paste0(args$cells, "_", suffix, ".tsv"),
+        row_names=FALSE,
+        col_names=FALSE
+    )
+    export_data(
+        umap_data,
+        location=paste0(args$umap, "_", suffix, ".tsv"),
+        row_names=FALSE,
+        col_names=FALSE
+    )
+    export_data(
+        cluster_data,
+        location=paste0(args$clusters, "_", suffix, ".tsv"),
+        row_names=FALSE,
+        col_names=FALSE
+    )
+}
+
+
 export_data <- function(data, location, row_names=FALSE, col_names=TRUE, quote=FALSE){
     tryCatch(
         expr = {
@@ -56,19 +90,31 @@ get_args <- function(){
         type="character", required="True"
     )
     parser$add_argument(
+        "--splitby",
+        help=paste(
+            "Column name to split data by. Barcode suffixes will be removed.",
+            "Output files will have values from the selected column as part of their names.",
+            "Default: do not split, export combined file"
+        ),
+        type="character"
+    )
+    parser$add_argument(
         "--cells",
-        help="Output filename to save cells information",
-        type="character", default="cells.tsv"
+        help="Output prefix to save cells information data as TSV file",
+        type="character", default="./cells"
     )
     parser$add_argument(
         "--umap",
-        help="Output filename to save UMAP embeddings",
-        type="character", default="umap.tsv"
+        help="Output prefix to save UMAP embeddings data as TSV file",
+        type="character", default="./umap"
     )
     parser$add_argument(
         "--clusters",
-        help="Output filename to save clusters selected by the --source parameter",
-        type="character", default="clusters.tsv"
+        help=paste(
+            "Output prefix to save clusters selected by the --source",
+            "parameter data as TSV file"
+        ),
+        type="character", default="./clusters"
     )
     args <- parser$parse_args(commandArgs(trailingOnly=TRUE))
     return (args)
@@ -80,18 +126,22 @@ args <- get_args()
 print(paste("Loading Seurat data from", args$rds))
 seurat_data <- readRDS(args$rds)
 
-cells_data <- Cells(seurat_data)
-print(paste("Loaded", length(cells_data), "cell"))
-print(head(cells_data))
-
-umap_data <- Embeddings(seurat_data, reduction="umap")
-print(paste("Loaded", length(umap_data), "UMAP embeddings"))
-print(head(umap_data))
-
-cluster_data <- seurat_data@meta.data[, args$source]
-print(paste("Loaded", length(cluster_data), "clusters"))
-print(head(cluster_data))
-
-export_data(cells_data, args$cells, row_names=FALSE, col_names=FALSE)
-export_data(umap_data, args$umap, row_names=FALSE, col_names=FALSE)
-export_data(cluster_data, args$clusters, row_names=FALSE, col_names=FALSE)
+if (!is.null(args$splitby)){
+    print(paste("Splitting Seurat data by", args$splitby))
+    Idents(seurat_data) <- args$splitby
+    identities <- unique(as.vector(as.character(Idents(seurat_data))))
+    for (i in 1:length(identities)) {
+        current_identity <- identities[i]
+        print(paste("Subsetting to", current_identity))
+        suffix <- gsub("'|\"| |-", "_", current_identity)
+        filtered_seurat_data <- subset(seurat_data, idents=current_identity)
+        filtered_seurat_data <- RenameCells(
+            filtered_seurat_data,
+            new.names=sapply( strsplit(Cells(filtered_seurat_data), "-", fixed=TRUE), "[", 1 )
+        )
+        export_metadata(filtered_seurat_data, suffix, args)
+    }
+} else {
+    print("Exporting no splitted data")
+    export_metadata(seurat_data, "combined", args)
+}
