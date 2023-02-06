725ba325bcf6b74ea18b8723481c15c956eaf886
Michael Kotliar <misha.kotliar@gmail.com>
2023-02-06 16:44:57 -0500

    Update volcano plot docker file, add MA-plot

--- tools/dockerfiles/scripts/ma_plot.sh
+++ tools/dockerfiles/scripts/ma_plot.sh
@@ -3,4 +3,4 @@
 echo "Copying volcano plot to the current directory"
 cp -r /opt/volcano_plot .
 cd ./volcano_plot
-./setVars.sh "$(basename -- $1)" $2 $3 $4 "chart" "sidebar" "volcano" "volcano_plot/html_data"
+./MA_PLOT_set_vars.sh "$(basename -- $1)" $2 $3 $4 "chart" "sideForm" "MA" "MD-MA_plot/html_data"
